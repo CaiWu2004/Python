d38899fc79f8b30b0d0d3f51f49aaa7968db38fa
CaiWu2004 <wcai19820@gmail.com>
2025-01-28 22:51:59 -0500

    Update Count Them Donuts.py

--- HW/Week 1/Count Them Donuts.py
+++ HW/Week 1/Count Them Donuts.py
@@ -1,3 +1,11 @@
+"""
+Author: [Harry Wu]
+Assignment / Part: HW1 - Q2
+Date due: January 30, 11:50 pm
+I pledge that I have completed this assignment without collaborating
+with anyone else, in conformance with the NYU School of Engineering Policies and Procedures on Academic Misconduct.
+"""
+
 dozen = 12
 yours = int(input("How many dozen? Please enter a whole number!"))
 donuts = int(yours) * int(dozen)
