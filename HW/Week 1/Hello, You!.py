93f8c5b78d41d2af0d650fd8e5cb7d48980ec386
CaiWu2004 <wcai19820@gmail.com>
2025-01-28 22:52:26 -0500

    Update Hello, You!.py

--- HW/Week 1/Hello, You!.py
+++ HW/Week 1/Hello, You!.py
@@ -1,3 +1,12 @@
+"""
+Author: [Harry Wu]
+Assignment / Part: HW1 - Q1
+Date due: January 30, 11:50 pm
+I pledge that I have completed this assignment without collaborating
+with anyone else, in conformance with the NYU School of engineering Policies and Procedures on Academic Misconduct.
+"""
+
+
 print("Welcome to CS_UY 1114")
 name = input("What is your name?")
 age = input("How old are you?")
