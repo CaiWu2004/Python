5e3c8ad4aa8bddabd9298573531bdefc7d73e126
CaiWu2004 <wcai19820@gmail.com>
2025-01-28 23:51:02 -0500

    adding homework

---
+++ HW/Week 1/Hello You.py
@@ -0,0 +1,13 @@
+"""
+Author: [Harry Wu]
+Assignment / Part: HW1 - Q1
+Date due: January 30, 11:50pm
+I pledge that I have completed this assignment without collaborating
+with anyone else, in conformance with the NYU School of engineering Policies and Procedures on Academic Misconduct.
+"""
+
+print("Welcome to CS_UY 1114")
+name = input("What is your name?")
+age = input("How old are you?")
+
+print(name,",is",age,"years old","and is taking CS_UY 1114.")
